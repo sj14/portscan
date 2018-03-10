420905a62cf545928021da737ec95dad25d73f8d
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2018-03-10 18:23:29 +0100

    enhanced code

--- portscan.go
+++ portscan.go
@@ -10,22 +10,25 @@ import (
 	"time"
 )
 
-const max = 65535
-
 var (
 	wg             sync.WaitGroup
 	timeoutFlag    *string
 	hostFlag       *string
+	startPortFlag  *int
+	endPortFlag    *int
+	pauseFlag      *string
 	listClosedFlag *bool
 )
 
-func main() {
+func init() {
+	const max = 65535
+
 	// Command Line Flags
 	hostFlag = flag.String("host", "localhost", "the host to scan")
 	timeoutFlag = flag.String("timeout", "1000ms", "timeout (e.g. 50ms or 1s)")
-	startPortFlag := flag.Int("start", 80, "the lower end to scan")
-	endPortFlag := flag.Int("end", -1, "the upper end to scan")
-	pauseFlag := flag.String("pause", "1ms", "pause after each port scan (e.g. 5ms)")
+	startPortFlag = flag.Int("start", 80, "the lower end to scan")
+	endPortFlag = flag.Int("end", -1, "the upper end to scan")
+	pauseFlag = flag.String("pause", "1ms", "pause after each scanned port (e.g. 5ms)")
 	listClosedFlag = flag.Bool("closed", false, "list closed ports (true/false)")
 
 	flag.Parse()
@@ -36,37 +39,40 @@ func main() {
 	}
 
 	if *startPortFlag < 0 || *startPortFlag > max {
-		log.Fatalf("start port out of range (should be between 0 and %d)\n", max)
+		log.Fatalf("ending port out of range (should be between 0 and %d)\n", max)
 	}
 	if *endPortFlag < 0 || *endPortFlag > max {
-		log.Fatalf("end port out of range (should be between 0 and %d)\n", max)
+		log.Fatalf("ending port out of range (should be between 0 and %d)\n", max)
 	}
 	if *endPortFlag < *startPortFlag {
-		log.Fatal("end port should be higher than start port")
+		log.Fatal("ending port must be greater than beginning port")
 	}
+}
 
+func main() {
 	startTime := time.Now()
+
+	pauseDuration, err := time.ParseDuration(*pauseFlag)
+	if err != nil {
+		log.Print(err)
+	}
 
 	for port := *startPortFlag; port <= *endPortFlag; port++ {
 		wg.Add(1)
 		go scan(port)
-		pauseDuration, err := time.ParseDuration(*pauseFlag)
-		if err != nil {
-			log.Print("wrong time input format?\n", err)
-		}
 		time.Sleep(pauseDuration)
 	}
+
 	wg.Wait()
 	scanDuration := time.Since(startTime)
-	fmt.Printf("scan finished in %v", scanDuration)
+	fmt.Printf("scan finished in %v\n", scanDuration)
 }
 
 func scan(port int) {
 	portString := strconv.Itoa(port)
 	timeoutDuration, err := time.ParseDuration(*timeoutFlag)
-
 	if err != nil {
-		log.Print("wrong time input format?\n", err)
+		log.Print(err)
 	}
 
 	conn, err := net.DialTimeout("tcp", *hostFlag+":"+portString, timeoutDuration)
